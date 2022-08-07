824ad43d0725c9a25b0d7015d7257e733240b954
vipinable <vipinable@gmail.com>
2022-08-07 15:12:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,19 @@ resource "aws_sns_topic_subscription" "emailTopicSubscription" {
   protocol  = "email"
   endpoint  = "vipinable@gmail.com"
 }
+
+resource "aws_iam_role" "iamahywheres3role" {
+  name = "iamahywheres3role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["sts:AssumeRole","sts:SetSourceIdentity","sts:TagSession"]
+        Effect = "Allow"
+        Principal = {
+          Service = "rolesanywhere.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
