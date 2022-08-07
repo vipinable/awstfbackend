168c8345a2b2987181a8ecafb865b42721a18279
vipinable <vipinable@gmail.com>
2022-08-07 15:16:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,4 +23,18 @@ resource "aws_iam_role" "iamahywheres3role" {
       },
     ]
   })
+  inline_policy {
+    name = "s3permissions"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["s3:GetObject*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
 }
